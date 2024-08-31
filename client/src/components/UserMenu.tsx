5fa6d42c731d2c4d13b70aac13d144733a569f19
RileyAlexis <MxRileyAlexis@gmail.com>
2024-08-31 10:08:04 -0500

    added conditional login button

--- client/src/components/UserMenu.tsx
+++ client/src/components/UserMenu.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { Avatar, Menu, MenuItem, IconButton } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { Avatar, Menu, MenuItem, IconButton, Button } from "@mui/material";
 
 //Actions
 import { toggleLoginScreen, toggleUserProfile } from "../redux/reducers/tinklOptionsReducer";
 
+//Types
+import { TinklRootState } from "../redux/types/TinklRootState";
+
+//CSS
+import '../App.css';
+
 export const UserMenu: React.FC = () => {
+    const user = useSelector((state: TinklRootState) => state.user);
+
     const [anchorEl, setAncorEl] = useState<null | HTMLElement>(null);
     const dispatch = useDispatch();
 
@@ -33,31 +41,41 @@ export const UserMenu: React.FC = () => {
     }
 
     return (
-        <div>
-            <IconButton onClick={handleMenuOpen}
-                sx={{
+        <div className="menuContainer">
+            {user.id !== 0 &&
+                <div>
+                    <IconButton onClick={handleMenuOpen}
+                        sx={{
 
-                    width: 50,
-                    height: 50,
-                }}
-            >
-                <Avatar alt="User Avatar"
-                    aria-label="User Account Menu"
-                    sx={{
-                        width: 28,
-                        height: 28,
-                    }}
-                />
-            </IconButton>
-            <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleMenuClose}
-            >
-                <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
-                <MenuItem onClick={handleLoginClick}>Login/Register</MenuItem>
-                <MenuItem onClick={handleLogoutClick}>Logout</MenuItem>
-            </Menu>
+                            width: 50,
+                            height: 50,
+                        }}
+                    >
+                        <Avatar alt="User Avatar"
+                            aria-label="User Account Menu"
+                            sx={{
+                                width: 28,
+                                height: 28,
+                            }}
+                        />
+                    </IconButton>
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleMenuClose}
+                    >
+                        <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
+                        {/* <MenuItem onClick={handleLoginClick}>Login/Register</MenuItem> */}
+                        <MenuItem onClick={handleLogoutClick}>Logout</MenuItem>
+                    </Menu>
+                </div>
+            }
+
+            {!user.id &&
+                <div id="loginButtonContainer">
+                    <Button size="small" variant="contained" onClick={handleLoginClick}>Log In</Button>
+                </div>
+            }
         </div>
     )
 }
