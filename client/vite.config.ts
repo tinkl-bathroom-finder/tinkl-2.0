33497f4fe91d74ff1cdff139dcc6c16aed47333f
RileyAlexis <MxRileyAlexis@gmail.com>
2024-09-13 14:39:45 -0500

    ts and vite config updates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    // host: '0.0.0.0',
     port: 5173,
     // https: {
     //   key: fs.readFileSync(path.resolve(__dirname, 'localhost.key')),
