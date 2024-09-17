09f2c0da0ff1d0742c6e61cb2f93079e914d4cb0
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-09-16 23:41:37 -0500

    Passed down selected bathroom to BathroomDetails component

--- client/src/components/BathroomDetails.tsx
+++ client/src/components/BathroomDetails.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BathroomType } from "../redux/types/BathroomType";
 
+import { TinklRootState } from "../redux/types/TinklRootState";
+
 interface BathroomDetailsProps {
     bathroom: BathroomType
 }
 
 export const BathroomDetails: React.FC<BathroomDetailsProps> = ({bathroom}) => {
-    console.log("We're in the BathroomDetails component!")
+    const options = useSelector((state: TinklRootState) => state.options);
+    const bathroomData: BathroomType[] = useSelector((state: TinklRootState) => state.bathroomData);
+    const selectedBathroom = bathroomData.filter(function(br){return br.id === options.selectedBathroomID})[0]
+    console.log("selectedBathroom", selectedBathroom)
     return (
         <div className="aboutContainer">
             <h1 className="aboutText">Here it is!</h1>
-            <h2>{bathroom.name}</h2>
+            <h2>{selectedBathroom.name}</h2>
+            <h3>{options.selectedBathroomID}</h3>
         </div>
     )
 }
