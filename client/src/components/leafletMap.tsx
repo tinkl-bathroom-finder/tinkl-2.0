ef5def1ef81d129c7851d921fa482f50ae599b75
Leo Johnson <leochinonejohnson@gmail.com>
2024-09-18 15:14:00 -0500

    created filter buttons and open filter functionality

--- client/src/components/leafletMap.tsx
+++ client/src/components/leafletMap.tsx
@@ -1,5 +1,5 @@
 // import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 //Map and Map Styling
 import _, { Icon } from 'leaflet';
@@ -10,9 +10,25 @@ import { MapLibreTileLayer } from './MapLibreTileLayer';
 import blueDotIconFile from './blue_dot.png';
 import toiletIconFile from './toilet-marker.png';
 
+//Filter Actions
+import {
+    toggleOpen,
+    togglePublic,
+    toggleAccessible,
+    toggleChangingTable,
+    clearFilters
+} from '../redux/reducers/bathroomFiltersReducer'
+
 //MUI
 import { Button } from '@mui/material';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
+import {
+    AccessibleForwardOutlined,
+    BabyChangingStationOutlined,
+    Man4,
+    Public,
+    TransgenderOutlined
+  } from "@mui/icons-material";
 
 //Types
 import { TinklRootState } from '../redux/types/TinklRootState';
@@ -45,8 +61,12 @@ import { OpenInMapsButton } from './OpenInMapsButton';
 // };
 
 export const LeafletMap = () => {
+    const dispatch = useDispatch()
+
     const user = useSelector((state: TinklRootState) => state.user);
     const options = useSelector((state: TinklRootState) => state.options);
+    const filters = useSelector((state: TinklRootState) => state.filters);
+
     const bathroomData: BathroomType[] = useSelector((state: TinklRootState) => state.bathroomData);
     const mapTilesURL = options.darkMode ? "https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json" : "https://tiles.stadiamaps.com/styles/osm_bright.json"
     // const center = user.location.lat && user.location.lng ? [user.location.lat, user.location.lng] : [44.9560534624369, -93.16002444658359];
@@ -100,7 +120,96 @@ export const LeafletMap = () => {
         )
     };
 
+    const FilterOpenButton: React.FC = () => {
+        const handleFilter = () => {
+            dispatch(toggleOpen())
+        };
+        return (<Button onClick={handleFilter} style={{
+            position: 'absolute',
+            top: '10px',
+            right: '10px',
+            height: '32px',
+            width: '101px',
+            border: '2px solid grey',
+            borderRadius: '1px',
+            backgroundColor: !filters.open ? 'white' : 'gray',
+            zIndex: 1000,
+            textTransform: 'lowercase'
+        }}
+        >
+            open now
+        </Button>
+        )
+    };
 
+    const FilterAccessibleButton: React.FC = () => {
+        const handleFilter = () => {
+            dispatch(toggleAccessible())
+        };
+        return (<Button onClick={handleFilter} style={{
+            position: 'absolute',
+            top: '41px',
+            right: '76px',
+            height: '32px',
+            minWidth: '0px',
+            width: '35px',
+            padding: '0',
+            border: '2px solid grey',
+            borderRadius: '1px',
+            backgroundColor: !filters.accessible ? 'white' : 'gray',
+            zIndex: 1000,
+        }}
+        >
+            <AccessibleForwardOutlined/>
+        </Button>
+        )
+    };
+
+    const FilterChangingButton: React.FC = () => {
+        const handleFilter = () => {
+            dispatch(toggleChangingTable())
+        };
+        return (<Button onClick={handleFilter} style={{
+            position: 'absolute',
+            top: '41px',
+            right: '43px',
+            height: '32px',
+            minWidth: '0px',
+            width: '35px',
+            padding: '0',
+            border: '2px solid grey',
+            borderRadius: '1px',
+            backgroundColor: !filters.changingTable ? 'white' : 'gray',
+            zIndex: 1000,
+        }}
+        >
+            <BabyChangingStationOutlined/>
+        </Button>
+        )
+    };
+
+    const FilterPublicButton: React.FC = () => {
+        const handleFilter = () => {
+            dispatch(togglePublic())
+        };
+        return (<Button onClick={handleFilter} style={{
+            position: 'absolute',
+            top: '41px',
+            right: '10px',
+            height: '32px',
+            minWidth: '0px',
+            width: '35px',
+            padding: '0',
+            border: '2px solid grey',
+            borderRadius: '1px',
+            backgroundColor: !filters.public ? 'white' : 'gray',
+            zIndex: 1000,
+        }}
+        >
+            <Public/>
+        </Button>
+        )
+    };
 
     return (
         <MapContainer center={user.location} zoom={13} style={{ height: "100%", width: "100%" }}>
@@ -118,20 +227,43 @@ export const LeafletMap = () => {
             />
 
             <RecenterButton />
+
+            <FilterOpenButton />
+            <FilterAccessibleButton />
+            <FilterChangingButton />
+            <FilterPublicButton />
+
             <Marker
                 position={user.location}
                 icon={blueDotIcon}
             >
                 {bathroomData.map((item, index) => {
-
                     return (
+                        // if no filters selected return all markers
+                        !filters.open && !filters.accessible && !filters.changingTable && !filters.public &&
                         <Marker
                             key={index}
                             position={[item.latitude, item.longitude]}
                             icon={item.is_open ? toiletIcon : toiletIconClosed}
                             alt={item.name}
                         >
+                            <Popup>
+                                <p>{item.name}</p>
+                                <p>{item.day_5_open} - {item.day_5_close}</p>
+                                <Button>Flag</Button>
+                                <Button>Like</Button>
+                                <OpenInMapsButton address={item.street} />
 
+                            </Popup>
+                        </Marker>
+                        // if open filter selec
+                        || filters.open && item.is_open &&
+                        <Marker
+                            key={index}
+                            position={[item.latitude, item.longitude]}
+                            icon={item.is_open ? toiletIcon : toiletIconClosed}
+                            alt={item.name}
+                        >
                             <Popup>
                                 <p>{item.name}</p>
                                 <p>{item.day_5_open} - {item.day_5_close}</p>
