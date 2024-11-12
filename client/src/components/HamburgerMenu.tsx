a3f883666ffeb38b06395180cbc1fc751eaef6ac
Paul LeRoy <Pleroy33@gmail.com>
2024-11-12 15:47:50 -0600

    building drawer

--- client/src/components/HamburgerMenu.tsx
+++ client/src/components/HamburgerMenu.tsx
@@ -1,59 +1,50 @@
 import * as React from "react";
-// import Box from '@mui/material/Box';
-// import Drawer from '@mui/material/Drawer';
+import { useState, } from "react";
+
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
-// import Divider from'@mui/material/Divider';
-// import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import { Book } from "@mui/icons-material";
 
-const options = [
-    'Find a bathroom',
-    'About TINKL',
-    'List View',
-    'Contact us',
-];
-export default function HamburgerMenu() {
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [selectedIndex, setSelectedIndex] = React.useState(1);
-    const open = Boolean(anchorEl);
-    const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
 
-    const handleMenuItemClick = (
-        event: React.MouseEvent<HTMLElement>,
-        index: number
-    ) => {
-        setSelectedIndex(index);
-        setAnchorEl(null);
-    };
+export default function AppHamburgerMenu() {
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+    const [open, setOpen] = useState(false);
+
+    const toggleDrawer = (newOpen: boolean) => () => {
+        setOpen(newOpen);
+    }
+
+    const DrawerList = (
+        <Box sx={{ width: 250 }}
+            role='presentation'
+            onClick={toggleDrawer(false)}
+        >
+            <List>
+                {['Find a bathroom', 'About', 'Add', 'List View', 'Contact Us']}
+
+            </List>
+        </Box>
+    )
 
     return (
         <div>
-            <List
-                component="nav"
-                aria-label="Navigation"
-                sx={{ bgcolor: 'background.paper' }}
+            <Button
+                id='Menu Button'
+                aria-controls={open ? 'menu' : undefined}
+                aria-haspopup='true'
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
             >
-                <ListItemButton>
-                    id='lock'
 
-                    <ListItemText>
+            </Button>
 
-                    </ListItemText>
-                </ListItemButton>
-
-
-
-            </List>
         </div>
     )
 }
