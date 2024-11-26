ba3d79bb0b3af958f662dc441c92c4c274fab229
RileyAlexis <MxRileyAlexis@gmail.com>
2024-11-26 12:46:03 -0600

    updated pool to connect to deployed postgres

--- server/src/pool.ts
+++ server/src/pool.ts
@@ -7,6 +7,14 @@ if (process.env.DATABASE_URL) {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
     });
+} else if (process.env.TEST_SERVER_DATABASE) {
+    pool = new Pool({
+        user: 'tinkl2app',
+        password: process.env.TEST_SERVER_DATABASE_PASSWORD,
+        host: 'localhost',
+        port: 5432,
+        database: 'bathrooms'
+    })
 }
 
 else {
